0f78e4fe900b5d202540e0d540f9fcb99c7e00a4
DrChrisHax <cmanlove1234@outlook.com>
2026-06-20 14:44:12 -0700

    Removed cloudflare adapter in favor of static page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://chrismanlove.com',
-  adapter: cloudflare(),
   integrations: [sitemap()],
 });
